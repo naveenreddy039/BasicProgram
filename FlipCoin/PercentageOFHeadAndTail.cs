037a9cd17e58078654b159be384150a212fff935
naveen <naveenreddy.k07@gmail.com>
2021-11-02 10:26:17 +0530

    first commit

---
+++ FlipCoin/PercentageOFHeadAndTail.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlipCoin
+{
+    public class PercentageOFHeadAndTail
+    {
+        public void printHeadAndTail()
+        {
+            int head = 0;
+            int tail = 0;
+            int CoinResult = 0;
+            Random random = new Random();
+
+            Console.WriteLine("Enter the input of Coin Flip");
+
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < input; i++)
+            {
+                CoinResult = random.Next(0, 2);
+                if (CoinResult == 0)
+                {
+                    head++;
+                }
+                else
+                {
+                    tail++;
+                }
+            }
+            double resultHead = (head * 100) / input;
+            double resultTail = (tail * 100) / input;
+            Console.WriteLine("Head is Flip" + resultHead);
+            Console.WriteLine("Tail is Flip" + resultTail);
+        }
+        
+    }
+}
---
+++ FlipCoin/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FlipCoin
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           //label: Console.WriteLine("1.Print the percentage coin Flip");
+           // Console.WriteLine("Type the option");
+            int i = int.Parse(Console.ReadLine());
+
+
+            //switch(i)
+            //{
+                //case 1:
+                    //Console.WriteLine("1.Print the percentage coin Flip");
+                    PercentageOFHeadAndTail percentageOFHeadAndTail = new PercentageOFHeadAndTail();
+                    percentageOFHeadAndTail.printHeadAndTail();
+                    //break;
+                //default:
+                   // Console.WriteLine("Invalid option");
+                    //break;
+            //}
+            //Console.WriteLine("Do you want to continue...yes/No");
+            //if(Console.ReadKey().KeyChar.ToString()=="yes")
+            //{
+                //goto label;
+            //}
+            Console.ReadKey();
+        }
+    }
+}
