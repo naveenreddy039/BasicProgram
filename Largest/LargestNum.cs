29a6299f422d83f78ffae93348dec72b69c3fac6
naveen <naveenreddy.k07@gmail.com>
2021-11-02 17:52:18 +0530

    second commit

---
+++ Largest/LargestNum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Largest
+{
+    public class LargestNum
+    {
+        public void printLarge()
+        {
+            Console.WriteLine("Enter the number1:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the number2:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the number3:");
+            int c = int.Parse(Console.ReadLine());
+            if(a>=b && a>=c)
+                Console.WriteLine(a + "is largest number");
+                
+                else if(b>=a && b>=c)
+                    Console.WriteLine(b + "is largest number");
+            else
+                Console.WriteLine(c + "is largest number");
+        }
+    }
+}
---
+++ Largest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Largest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LargestNum largestNum = new LargestNum();
+            largestNum.printLarge();
+            Console.ReadKey();
+        }
+    }
+}
