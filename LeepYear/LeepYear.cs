904a8194f52fbb92062167d2206a009c2becc560
naveen <naveenreddy.k07@gmail.com>
2021-11-02 11:04:22 +0530

    welcome

---
+++ LeepYear/LeepYear.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeepYear
+{
+    public class LeepYear
+    {
+        public void printYear()
+        {
+            Console.WriteLine("Enther the year");
+            int a = int.Parse(Console.ReadLine());
+            bool year = false;
+            if (a % 4 == 0)
+            {
+                if (a % 100 == 0)
+                {
+                    if (a % 400 == 0)
+                    {
+                        year = true;
+                    }
+                    else
+                    {
+                        year = false;
+                    }
+                }
+                else
+                {
+                    year = true;
+                }
+            }
+            else
+            {
+                year = false;
+            }
+            if (year)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
---
+++ LeepYear/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LeepYear
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LeepYear leepYear = new LeepYear();
+            leepYear.printYear();
+            Console.ReadKey();
+        }
+    }
+}
