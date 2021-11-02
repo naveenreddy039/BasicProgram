a93fa626edc4413389d8ff7ca530a557749d92cd
naveen <naveenreddy.k07@gmail.com>
2021-11-02 12:35:32 +0530

    second commit

---
+++ Factorial/Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factorial
+{
+    public class Factorial
+    {
+        public void printFact()
+        {
+            Console.WriteLine("Enter then number:");
+            int num = int.Parse(Console.ReadLine());
+            int i;
+            for(i=1;i<=num;i++)
+            {
+                if(num%i==0)
+                {
+                    Console.WriteLine(i + " ");
+                }
+            }
+        }
+    }
+}
---
+++ Factorial/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Factorial
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Factorial factorial = new Factorial();
+            factorial.printFact();
+            Console.ReadKey();
+        }
+    }
+}
