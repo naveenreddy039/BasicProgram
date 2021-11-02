b25b39ad75251b9b41cacbae244363ddcc9e3173
naveen <naveenreddy.k07@gmail.com>
2021-11-02 12:00:24 +0530

    second commit

---
+++ Harmonic/Harmonic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harmonic
+{
+    public class Harmonic
+    {
+        public void printHarm()
+        {
+            Console.WriteLine("enter the number:");
+            int num = int.Parse(Console.ReadLine());
+            double i, s = 0.0;
+            for(i=1;i<=num;i++)
+            {
+                s = s + 1 / i;
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ Harmonic/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Harmonic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Harmonic harmonic = new Harmonic();
+            harmonic.printHarm();
+        }
+    }
+}
