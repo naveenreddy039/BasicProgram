e62aa1ce0da10f7cafb8bac427cbb1149e673478
naveen <naveenreddy.k07@gmail.com>
2021-11-02 16:30:01 +0530

    second commit

---
+++ EvenOdd/EvenOddNum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvenOdd
+{
+    public class EvenOddNum
+    {
+        public void printEvenNum()
+        {
+            Console.WriteLine("Enter the number:");
+            int num = int.Parse(Console.ReadLine());
+            if(num%2==0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+}
---
+++ EvenOdd/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EvenOdd
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EvenOddNum evenOddNum = new EvenOddNum();
+            evenOddNum.printEvenNum();
+        }
+    }
+}
