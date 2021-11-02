f03dfc982a5b40149fb00b20d6fa587181297dd5
naveen <naveenreddy.k07@gmail.com>
2021-11-02 17:26:11 +0530

    first commit

---
+++ Alphabet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Alphabet
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            VowelConsnt vowelConsnt = new VowelConsnt();
+            vowelConsnt.printVowel();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Alphabet/VowelConsnt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alphabet
+{
+    public class VowelConsnt
+    {
+        public void printVowel()
+        {
+            Console.WriteLine("Enter the alphabet:");
+            char ch = char.Parse(Console.ReadLine());
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'|| ch == 'E'|| ch == 'I'|| ch == 'O'|| ch == 'U' )
+                    {
+                Console.WriteLine("Alphabet is Vowel");
+            }
+            else if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                Console.WriteLine("Alphabet is Consonant");
+            }
+            else
+            {
+                Console.WriteLine("Is not Alphabet");
+            }
+        }
+    }
+}
