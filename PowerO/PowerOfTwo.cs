be0eec0c786dcb7cad4bb10f6606c1a5cca22df6
naveen <naveenreddy.k07@gmail.com>
2021-11-02 11:33:34 +0530

    second commit

---
+++ PowerO/PowerOfTwo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowerO
+{
+    public class PowerOfTwo
+    {
+        public void printTwo()
+        {
+            Console.WriteLine("Enter th number:");
+            int n = int.Parse(Console.ReadLine());
+            
+            int i = 0;
+            int powerOfTwo = 1;
+            while(i<=n)
+            {
+                Console.WriteLine(i + " " + powerOfTwo);
+                powerOfTwo = 2 * powerOfTwo;
+                i = i + 1;
+            }
+
+        }
+    }
+}
---
+++ PowerO/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PowerO
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PowerOfTwo powerOfTwo = new PowerOfTwo();
+            powerOfTwo.printTwo();
+            Console.ReadKey();
+        }
+    }
+}
