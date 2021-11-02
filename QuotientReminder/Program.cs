58faa3fc0b5fe7cb9208c660aba6369a4e08bddb
naveen <naveenreddy.k07@gmail.com>
2021-11-02 13:24:32 +0530

    third commit

---
+++ QuotientReminder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QuotientReminder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            QuesReme quesReme = new QuesReme();
+            quesReme.printQurem();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QuotientReminder/QuesReme.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuotientReminder
+{
+    public class QuesReme
+    {
+        public void printQurem()
+        {
+            Console.WriteLine("Enter the dividend:");
+            int dividend = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the divisor:");
+            int divisor = int.Parse(Console.ReadLine());
+
+            int reminder = dividend % divisor;
+            int quotient = dividend / divisor;
+            Console.WriteLine("reminder:"+reminder);
+            Console.WriteLine("quotient:"+quotient);
+        }
+    }
+}
