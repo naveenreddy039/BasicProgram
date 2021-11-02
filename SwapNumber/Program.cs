8c259e231d3a83f6114d71932e06780eeeb64413
naveen <naveenreddy.k07@gmail.com>
2021-11-02 16:13:52 +0530

    first commit

---
+++ SwapNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SwapNumber
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SwapNum swapNum = new SwapNum();
+            swapNum.printNum();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ SwapNumber/SwapNum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwapNumber
+{
+    public class SwapNum
+    {
+        public void printNum()
+        {
+            Console.WriteLine("Enter the first number:");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            int n2 = int.Parse(Console.ReadLine());
+            int temp = n1;
+            n1 = n2;
+            n2 = temp;
+            Console.WriteLine(n1 + " " + n2);
+        }
+    }
+}
